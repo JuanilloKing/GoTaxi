918fefc119ad41f9076e19addcb96ff213e69292
Eduardo <eduardosumarivasalgado@gmail.com>
2025-06-08 17:54:06 +0200

    aniadido footer en edit perfil

--- resources/js/Pages/Profile/Edit.jsx
+++ resources/js/Pages/Profile/Edit.jsx
@@ -4,6 +4,7 @@ import DeleteUserForm from './Partials/DeleteUserForm';
 import UpdatePasswordForm from './Partials/UpdatePasswordForm';
 import UpdateProfileInformationForm from './Partials/UpdateProfileInformationForm';
 import Header from '@/Components/Header'; // Importa el nuevo Header
+import Footer from '@/Components/Footer';
 
 export default function Edit({ mustVerifyEmail, status }) {
     const { auth } = usePage().props;
@@ -11,29 +12,31 @@ export default function Edit({ mustVerifyEmail, status }) {
 
     return (
         <div>
-            <Header  isLoggedIn={isLoggedIn} />
+            <Header isLoggedIn={isLoggedIn} />
 
             <Head title="Profile" />
 
             <div className="py-12">
                 <div className="mx-auto max-w-7xl space-y-6 sm:px-6 lg:px-8">
-                    <div className="bg-white p-4 shadow sm:rounded-lg sm:p-8">
+                    <div className="bg-white p-4 shadow sm:rounded-lg sm:p-8 flex justify-center">
                         <UpdateProfileInformationForm
                             mustVerifyEmail={mustVerifyEmail}
                             status={status}
-                            className="max-w-xl"
+                            className="w-full max-w-xl"
                         />
                     </div>
 
-                    <div className="bg-white p-4 shadow sm:rounded-lg sm:p-8">
-                        <UpdatePasswordForm className="max-w-xl" />
+                    <div className="bg-white p-4 shadow sm:rounded-lg sm:p-8 flex justify-center">
+                        <UpdatePasswordForm className="w-full max-w-xl" />
                     </div>
 
-                    <div className="bg-white p-4 shadow sm:rounded-lg sm:p-8">
-                        <DeleteUserForm className="max-w-xl" />
+                    <div className="bg-white p-4 shadow sm:rounded-lg sm:p-8 flex justify-center">
+                        <DeleteUserForm className="w-full max-w-xl" />
                     </div>
                 </div>
             </div>
+
+            <Footer />
         </div>
     );
 }
